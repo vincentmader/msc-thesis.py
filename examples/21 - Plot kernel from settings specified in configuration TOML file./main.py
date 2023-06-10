ac374825bf3effa1321bd766b28931c2711a2179
Vincent C. Mader <vincent.mader@icloud.com>
2023-06-10 17:48:27 +0200

    added kernel gain-vs-loss plot

--- examples/21 - Plot kernel from settings specified in configuration TOML file./main.py
+++ examples/21 - Plot kernel from settings specified in configuration TOML file./main.py
@@ -5,6 +5,7 @@ try:
     from config import Config
     from kernel import Kernel
     from visualization.kernel.v1.interactive_kernel_layer_plot import InteractiveKernelLayerPlot
+    from visualization.kernel.v2.kernel_gain_vs_loss import KernelGainVsLossPlot
 except ModuleNotFoundError as e:
     raise e
 
@@ -29,4 +30,7 @@ if __name__ == "__main__":
         kernel_subplot_titles=["$K_{kij}$"],
         cmap_limits=(-1, 1),
     )
+    p.show()
+
+    p = KernelGainVsLossPlot(kernel)
     p.show()
