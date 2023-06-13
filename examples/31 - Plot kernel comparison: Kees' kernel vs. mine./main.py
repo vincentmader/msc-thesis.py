5c2693c44a77e77b60b9007e23950f3cdb7a7b0d
Vincent C. Mader <vincent.mader@icloud.com>
2023-06-13 09:48:23 +0200

    minor: removed unneeded import

--- examples/31 - Plot kernel comparison: Kees' kernel vs. mine./main.py
+++ examples/31 - Plot kernel comparison: Kees' kernel vs. mine./main.py
@@ -8,7 +8,6 @@ try:
     from kees_kernel import create_coag_kernel
     from kernel import Kernel
     from visualization.kernel.v1.interactive_kernel_layer_plot import InteractiveKernelLayerPlot
-    from visualization.kernel.v2.kernel_gain_vs_loss import KernelGainVsLossPlot
 except ModuleNotFoundError as e:
     raise e
 
