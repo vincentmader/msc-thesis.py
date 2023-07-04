485a7352c61e8bf091b816ac0682ebfd8d2ada10
Vincent C. Mader <vincent.mader@icloud.com>
2023-07-04 10:58:05 +0200

    added possibility to plot mass error vs. time not as error/timestep but error/year

--- src/visualization/mass_error.py
+++ src/visualization/mass_error.py
@@ -24,8 +24,13 @@ class DiskMassErrorPlot():
 
     def draw(self):
         x = self.x / (365 * 24 * 60 * 60)
-        self.ax_1.loglog(x, self.y, 'r', label="pos.")
-        self.ax_1.loglog(x, -self.y, 'b', label="neg.")
+        y = self.y
+
+        # y = y[:-1] / (x[1:] - x[:-1])
+        # x = x[:-1]
+
+        self.ax_1.loglog(x, y, 'r', label="pos.")
+        self.ax_1.loglog(x, -y, 'b', label="neg.")
         # TODO ^ Draw as semilogy for linear time axis.
         plt.title(r"mass error $\frac{m-m_0}{m_0}$")
         plt.ylabel("$n_i$ [kg$^{-1}$ $m^{-3}$ $s^{-1}}$]")
