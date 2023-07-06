293e6bf2093511909bdc3784ff0f58239acf31ae
Vincent C. Mader <vincent.mader@icloud.com>
2023-07-06 23:35:50 +0200

    minor: renamed file

--- src/collision/__init__.py
+++ src/collision/__init__.py
@@ -1,3 +1,3 @@
 from .collision_rate import collision_rate
 from .collision_cross_section import collision_cross_section
-from .outcome_probabilities import collision_outcome_probabilities_from_maxwell_boltzmann
+from .collision_outcome import collision_outcome_probabilities_from_maxwell_boltzmann
