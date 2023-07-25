e9a469f1eba8523b822826da2768f793a017deaa
Vincent C. Mader <vincent.mader@icloud.com>
2023-07-26 01:12:57 +0200

    MINOR: Add assertion for probabilities, must add up to 1

--- src/collision/collision_outcome.py
+++ src/collision/collision_outcome.py
@@ -17,8 +17,11 @@ def collision_outcome_probabilities_from_maxwell_boltzmann(
             # Calculate fragmentation probability.
             P_f = (3/2 * (v_frag/dv[i, j])**2 + 1) * np.exp(-3/2 * (v_frag/dv[i, j])**2)
             # ^ See "2022 Stammler & Birnstiel"
-            P_frag[i, j] = P_f
+            P_frag[i, j] = P_f 
             P_coag[i, j] = 1 - P_f  # NOTE: Bouncing is neglected here.
+
+    P_tot = np.sum(P_coag + P_frag) / N_m**2
+    assert P_tot == 1
 
     return P_coag, P_frag
 
@@ -40,5 +43,8 @@ def collision_outcome_probabilities_from_cutoff_velocity(
                 P_frag[i, j] = 1
             else:  # NOTE: Bouncing is neglected here.
                 P_coag[i, j] = 1
+
+    P_tot = np.sum(P_coag + P_frag) / N_m**2
+    assert P_tot == 1
 
     return P_coag, P_frag
