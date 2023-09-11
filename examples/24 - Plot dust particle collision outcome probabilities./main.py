d7a1f6464c7eb9122e82a4923de71f84f7eb66f4
Vincent C. Mader <vincent.mader@icloud.com>
2023-09-11 21:19:01 +0200

    IMPR: Separate plotting of coagulation & fragmentation probabilities

--- examples/24 - Plot dust particle collision outcome probabilities./main.py
+++ examples/24 - Plot dust particle collision outcome probabilities./main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from pathlib import Path
+import matplotlib.pyplot as plt
 try:
     sys.path.append(os.path.join("..", "..", "src"))
     from axis import DiscreteMassAxis, DiscreteRadialAxis
@@ -37,13 +38,7 @@ outcome_probabilities = {
     "Maxwell-Boltzmann": collision_outcome_probabilities_from_maxwell_boltzmann,
 }
 
-for title, outcome_probability in outcome_probabilities.items():
-    P_coag, P_frag = outcome_probability(cfg, dv)
-    outcome_probabilities = {
-        "P_coag": P_coag,
-        "P_frag": P_frag,
-    }
-
+def plot_together(title, P_coag, P_frag):
     s1 = PcolorMatrixSubplot(
         ac, ac, P_coag,
         title="dust particle coagulation probability $P_{coag}$",
@@ -74,16 +69,55 @@ for title, outcome_probability in outcome_probabilities.items():
         path_to_outfile=path_to_outfile
     )
 
+def plot_separately(title, P_coag, P_frag):
+    s = PcolorMatrixSubplot(
+        ac, ac, P_coag,
+        title="dust particle coagulation probability $P_{coag}$",
+        xlabel="particle radius $a_j$ [m]",
+        ylabel="particle radius $a_i$ [m]",
+        scales=("log", "log", "lin"),
+        cmap="Blues",
+    ) 
+    
+    path_to_figures = Path(PATH_TO_FIGURES, "24")
+    os.makedirs(path_to_figures, exist_ok=True)
+    filename = f"coagulation_probability_from_{title}.pdf"
+    path_to_outfile = Path(path_to_figures, filename)
+    
+    p = GridspecPlot([s])
+    p.render(
+        save_plot=True,
+        path_to_outfile=path_to_outfile
+    )
 
+    s = PcolorMatrixSubplot(
+        ac, ac, P_frag,
+        title="dust particle fragmentation probability $P_{frag}$",
+        xlabel="particle radius $a_j$ [m]",
+        ylabel="particle radius $a_i$ [m]",
+        scales=("log", "log", "lin"),
+        cmap="Blues",
+    )
+
+    path_to_figures = Path(PATH_TO_FIGURES, "24")
+    os.makedirs(path_to_figures, exist_ok=True)
+    filename = f"fragmentation_probability_from_{title}.pdf"
+    path_to_outfile = Path(path_to_figures, filename)
+    
+    p = GridspecPlot([s])
+    p.render(
+        save_plot=True,
+        path_to_outfile=path_to_outfile
+    )
+
+def plot_3(ac, P_coag, P_frag):
     k = 0
     x = ac
     y1 = P_frag[k]
     y2 = P_coag[k]
 
-    import matplotlib.pyplot as plt
     ax = plt.subplot()
     ax.set_xscale('log')
-
     # plt.scatter(x, y1)
     # plt.scatter(x, y2)
     # plt.plot(x, y1)
@@ -95,9 +129,20 @@ for title, outcome_probability in outcome_probabilities.items():
     )
 
     plt.legend(loc="upper right")
-
     plt.xlim(ac[0], ac[-1])
     plt.ylim(0, 1)
-
     plt.show()
     plt.close()
+
+
+for title, outcome_probability in outcome_probabilities.items():
+    P_coag, P_frag = outcome_probability(cfg, dv)
+    outcome_probabilities = {
+        "P_coag": P_coag,
+        "P_frag": P_frag,
+    }
+
+    plot_together(title, P_coag, P_frag)
+    plot_separately(title, P_coag, P_frag)
+    plot_3(ac, P_coag, P_frag)
+
