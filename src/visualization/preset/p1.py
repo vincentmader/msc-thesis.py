65d0a1ab3704428da3c2522e03c1b597d2aabd7d
Vincent C. Mader <vincent.mader@icloud.com>
2023-09-17 20:40:39 +0200

    MINOR

--- src/visualization/preset/p1.py
+++ src/visualization/preset/p1.py
@@ -139,21 +139,21 @@ def plot_error(
 
 def main(cfg):
     mg, kernel = DiscreteMassAxis(cfg), Kernel(cfg)
-
     scale = mg.scale
     axis = KernelAxis.Radius if scale == "log" else KernelAxis.Bin
     z_limits = (1e-20, 1e-7) if scale == "log" else (-1, 1)
-    # TODO Plot total kernel     with lin. colorscale.
+
+    # Plot total kernel     with lin. colorscale.
     plot_kernel(cfg, mg, kernel, scale, axis, z_limits)
-    # TODO Plot K_gain & K_loss  with log. colorscale.
+    # Plot K_gain & K_loss  with log. colorscale.
     plot_kernel_gain_loss(cfg, mg, kernel, scale, axis, z_limits)
-    # TODO Plot kernel mass error.
+    # Plot kernel mass error.
     plot_kernel_error(cfg, mg, kernel, scale, axis, z_limits)
 
-    # TODO Integrate.
+    # Integrate.
     t, f, N, m2f, dm2f, M = integrate(cfg, kernel)
 
-    # TODO Plot evolution of mass distribution over time.
+    # Plot evolution of mass distribution over time.
     plot_evolution(cfg, mg, kernel, f, N, m2f, dm2f)
-    # TODO Plot mass error over time.
+    # Plot mass error over time.
     plot_error(cfg, mg, kernel, t, M)
