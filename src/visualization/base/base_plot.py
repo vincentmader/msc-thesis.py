07deea3199d2c4d4a38dc038104d7c93cf915d18
Vincent C. Mader <vincent.mader@icloud.com>
2023-09-18 02:17:18 +0200

    FEAT: Add limit specification to `BasePlot` class

--- src/visualization/base/base_plot.py
+++ src/visualization/base/base_plot.py
@@ -4,13 +4,17 @@ import matplotlib.pyplot as plt
 
 
 class BasePlot:
-    __slots__ = ["fig"]
+    __slots__ = ["fig", "xlims", "ylims"]
 
     def __init__(
         self,
         figsize: Optional[tuple[int, int]] = None,
+        xlims: Optional[tuple[int, int]] = None,
+        ylims: Optional[tuple[int, int]] = None,
     ):
         self.fig = plt.figure(figsize=figsize)
+        self.xlims = xlims
+        self.ylims = ylims
 
     def render(
         self,
@@ -18,6 +22,10 @@ class BasePlot:
         save_plot=False,
         path_to_outfile=None,
     ):
+        if self.xlims is not None:
+            plt.xlim(self.xlims[0], self.xlims[1])
+        if self.ylims is not None:
+            plt.ylim(self.ylims[0], self.ylims[1])
         self.draw()
         if save_plot:
             assert path_to_outfile is not None, "No savefile path specified."
