3f28079fbacfa709c7915610d620d7c5d213504f
Vincent C. Mader <vincent.mader@icloud.com>
2023-09-18 21:16:38 +0200

    MINOR

--- bin/create_directories.sh
+++ bin/create_directories.sh
@@ -4,17 +4,17 @@ BLUE="\033[0;34m"
 NC="\033[0m"
 
 directories=(
-    "../lib"
     "../out"
 )
 
 msg="Creating directories..."
 echo "\n${BLUE}${msg}${NC}"
 for dir in "${directories[@]}"; do
-    echo "\`${dir}\`"
+    printf "\`${dir}\`"
     if [ -d "${dir}" ]; then
-        printf " -> Already exists."
+        echo " -> Already exists."
     else
         mkdir "${dir}"
+        echo
     fi
 done
