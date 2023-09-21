da6b62aa7280e94f3c5ba5771215030bad007185
Vincent C. Mader <vincent.mader@icloud.com>
2023-09-21 12:35:20 +0200

    MINOR: Increase resolution & max. value of mass grid, + minor refactoring

--- examples/34 - Compare kernels: with cancellation handling vs. without./main.py
+++ examples/34 - Compare kernels: with cancellation handling vs. without./main.py
@@ -5,17 +5,18 @@ try:
     sys.path.append(os.path.join("..", "..", "src"))
     from axis import KernelAxis
     from config import Config, PATH_TO_FIGURES
-    from dust import particle_mass_from_radius
     from kernel import Kernel
-    from kernel.mass_conservation import test_mass_conservation
     from visualization.base import GridspecPlot
     from visualization.kernel import KernelMassConservationSubplot, KernelSubplot
 except ModuleNotFoundError as e:
     raise e
 
-cfg_1 = Config(enable_cancellation_handling=False)
-cfg_2 = Config(enable_cancellation_handling=True)
-rho_s = cfg_1.dust_particle_density
+kwargs = {
+    "mass_max_value": 1e10, 
+    "mass_resolution": 100
+}
+cfg_1 = Config(enable_cancellation_handling=False, **kwargs)
+cfg_2 = Config(enable_cancellation_handling=True, **kwargs)
 kernel_1, kernel_2 = Kernel(cfg_1), Kernel(cfg_2)
 K_1, K_2 = kernel_1.K, kernel_2.K
 K_diff = K_1 - K_2
@@ -28,6 +29,7 @@ ac = mg.particle_radii
 
 def plot_1():
 
+    # Plot kernels `K_canc` `K_nocanc` side by side, & plot difference.
     GridspecPlot([
         KernelSubplot(
             mg, K_1,
@@ -53,6 +55,7 @@ def plot_1():
 
 def plot_2():
 
+    # Plot kernel errors of `K_canc` & `K_nocanc` side by side.
     s1 = KernelMassConservationSubplot(
         mg, K_1, axis=KernelAxis.Radius,
         title=r"kernel error $\Delta K^{canc}_{ij}=\sum_k m_k\cdot K_{kij}^{canc}$",
@@ -63,6 +66,7 @@ def plot_2():
     )
     GridspecPlot([s1, s2]).render()
 
+    # Plot kernel errors of `K_canc` & `K_nocanc` separately, one after the other.
     for label, subplot in zip(["canc", "nocanc"], [s1, s2]):
         path = Path(PATH_TO_FIGURES, "34")
         os.makedirs(path, exist_ok=True)
