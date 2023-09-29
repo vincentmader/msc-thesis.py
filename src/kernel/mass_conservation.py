424197e8da266798521e4232bb043b82cb1764ef
Vincent C. Mader <vincent.mader@icloud.com>
2023-09-29 20:47:49 +0200

    CLEAN Restructure code to be more readable

--- src/kernel/mass_conservation.py
+++ src/kernel/mass_conservation.py
@@ -17,21 +17,19 @@ def test_mass_conservation(mg, K):  # TODO Move to `Kernel` definition?
     assert N_m == mg.N
 
     mc, dm = mg.bin_centers, mg.bin_widths
-    err_matrix = np.zeros(shape=[N_m] * 2)
+    if mg.scale == "lin":
+        msg = f"dm_k != 1 on linear grid, is this on purpose?"
+        assert np.abs(dm - 1).all() == 0, msg
 
+    err_matrix = np.zeros(shape=[N_m] * 2)
     for i in range(N_m):
         for j in range(N_m):
             m_tot = mc[i] + mc[j]
 
             err_ij = 0
             for k in range(N_m):
-                if mg.scale == "lin":
-                    msg = f"dm_k != 1 on linear grid, is this on purpose? ({k=}, {dm[k]=})"
-                    assert np.abs(dm[k] - 1) <= 1e-14, msg
                 err_ij += mc[k] * K[k, i, j]
             err_matrix[i, j] = abs(err_ij) / m_tot # < 1e-12
 
     err_total = np.sum(err_matrix)  # TODO Calc. with squares?
-    print(err_total)
-
     return err_matrix, err_total
