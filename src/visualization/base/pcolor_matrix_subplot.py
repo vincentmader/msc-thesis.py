4c16170f1adf41e7e02b97fbec2b823d4b9c2501
Vincent C. Mader <vincent.mader@icloud.com>
2023-09-29 21:08:23 +0200

    MINOR Move z-axis norm definition in 3D matrix plots to separate method

--- src/visualization/base/pcolor_matrix_subplot.py
+++ src/visualization/base/pcolor_matrix_subplot.py
@@ -20,9 +20,6 @@ class PcolorMatrixSubplot(GridspecSubplot):
         y: np.ndarray,
         z: np.ndarray,
         k: Optional[int] = None,
-        title: Optional[str] = None,
-        xlabel: Optional[str] = None,
-        ylabel: Optional[str] = None,
         symmetrized: bool = False,
         scales: tuple[str, str, str] = ("log", "log", "log"), # TODO rename?
         cmap: str = "Reds",
@@ -30,72 +27,42 @@ class PcolorMatrixSubplot(GridspecSubplot):
         grid: Optional[bool] = False,
         xticks: Optional[Any] = None,
         yticks: Optional[Any] = None,
+        *args, **kwargs,
     ):
+        # Assert valid scale specifications, & valid (cubic) matrix shape.
         for scale in scales:
             assert scale in ["lin", "log"], "Invalid scale."
-
         if len(z.shape) < 3:
-            assert k is None, "2D matrix has no third index k."
-            assert z.shape[0] == z.shape[1], "Non-cubic kernel shape."
+            assert k is None, "2D matrix has no third index k, no need to specify it."
+            assert z.shape[0] == z.shape[1], "Matrix shape should be cubic."
         else:
-            assert z.shape[0] == z.shape[1] == z.shape[2], "Non-cubic kernel shape."
+            assert z.shape[0] == z.shape[1] == z.shape[2], "Matrix shape should be cubic."
 
-        super().__init__(
-            title=title,
-            xlabel=xlabel,
-            ylabel=ylabel,
-        )
+        # Initialize class instance.
+        super().__init__(*args, **kwargs)
+        self.scales, self.z_limits = scales, z_limits
+        self.cmap, self.grid = cmap, grid
+        self.xticks, self.yticks = xticks, yticks
         self.x, self.y, self.z = x, y, z
 
-        if symmetrized:
-            if len(z.shape) == 2:
-                self.z = 0.5 * (z + z.T) 
-            elif len(z.shape) == 3:
-                self.z = np.array([0.5 * (z + z.T) for z in z])
-            else:
-                raise Exception("")
+        # Symmetrize 3D matrix "layers".
+        if symmetrized and len(z.shape) == 2:
+            self.z = (z + z.T) / 2
+        if symmetrized and len(z.shape) == 3:
+            self.z = np.array([(z + z.T)/2 for z in z])
 
+        # Define initial matrix "layer index".
         if len(z.shape) < 3:
             self.k = None
         else:
             self.k = k if k is not None else z.shape[2] // 2
-        self.scales = scales
-        self.cmap = cmap
-        self.z_limits = z_limits
-        self.grid = grid
-        self.xticks = xticks
-        self.yticks = yticks
 
     def draw(self, axes):
         k, x, y = self.k, self.x, self.y
         z = self.z if k is None else self.z[k]
 
-        if self.scales[2] == "lin":
-            if self.z_limits is None:
-                vmin, vmax = z.min(), z.max()
-            else:
-                vmin = self.z_limits[0]
-                vmax = self.z_limits[1]
-            smin, smax = np.sign(vmin), np.sign(vmax)
-            if smin == smax or 0 in [smin, smax]:
-                norm = colors.Normalize(vmin=vmin, vmax=vmax)
-            else:
-                norm = colors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
-        elif self.scales[2] == "log":
-            if self.z_limits is None:
-                if z.min() == z.max() == 0:
-                    vmin = 1e-20
-                    vmax = 1e-7
-                    print(f"Reverting to [{vmin}, {vmax}]")
-                else:
-                    vmin = z.min() if z.min() != 0 else z[z != 0].min()
-                    vmax = z.max() if z.max() != 0 else z[z != 0].max()
-            else:
-                vmin = self.z_limits[0]
-                vmax = self.z_limits[1]
-            norm = colors.LogNorm(vmin=vmin, vmax=vmax)
-        else:
-            raise Exception("")
+        # Define norm.
+        norm = self._pcolormesh_norm(z)
 
         ax = axes[1]
         plt.sca(ax)
@@ -131,3 +98,31 @@ class PcolorMatrixSubplot(GridspecSubplot):
     def format_coord(self, x, y):
         # TODO: Redefine.
         return f"{x}, {y}"
+
+    def _pcolormesh_norm(self, z):
+        if self.scales[2] == "lin":
+            if self.z_limits is None:
+                vmin, vmax = z.min(), z.max()
+            else:
+                vmin, vmax = self.z_limits
+            smin, smax = np.sign(vmin), np.sign(vmax)
+            if smin == smax or 0 in [smin, smax]:
+                norm = colors.Normalize(vmin=vmin, vmax=vmax)
+            else:
+                norm = colors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
+        elif self.scales[2] == "log":
+            if self.z_limits is None:
+                if z.min() == z.max() == 0:
+                    vmin = 1e-20
+                    vmax = 1e-7
+                    print(f"Reverting to [{vmin}, {vmax}]")
+                else:
+                    vmin = z.min() if z.min() != 0 else z[z != 0].min()
+                    vmax = z.max() if z.max() != 0 else z[z != 0].max()
+            else:
+                vmin = self.z_limits[0]
+                vmax = self.z_limits[1]
+            norm = colors.LogNorm(vmin=vmin, vmax=vmax)
+        else:
+            raise Exception("")
+        return norm
