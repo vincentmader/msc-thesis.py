f0746a35fe70bc3d1c59a9807ba7f761d7b0fe42
Vincent C. Mader <vincent.mader@icloud.com>
2023-10-04 15:24:43 +0200

    FIX Update title of kernel mass error plot

--- src/visualization/kernel/kernel_mass_conservation_subplot.py
+++ src/visualization/kernel/kernel_mass_conservation_subplot.py
@@ -23,7 +23,7 @@ class KernelMassConservationSubplot(KernelSubplot):
         err_matrix, err_total = test_mass_conservation(mg, K)
 
         title = r"error $\Delta K_{ij}=\sum_k \frac{m_k}{m_i+m_j}\cdot K_{kij}$, "
-        title += r"$\Delta K=\sum_{ij}|\Delta K_{ij}|$ = " 
+        title += r"$\Delta K=\sqrt{\sum_{ij}\Delta K_{ij}^2}$ = " 
         title += f"{err_total:.2e}" + " m$^3$s$^{-1}$"
 
         kwargs["title"] = kwargs["title"] if "title" in kwargs.keys() else title
