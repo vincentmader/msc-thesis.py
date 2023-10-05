4504659f207ce9ed7966900a56d8e310d47b5ce3
Vincent C. Mader <vincent.mader@icloud.com>
2023-10-05 22:40:36 +0200

    MINOR

--- src/kernel/kernel.py
+++ src/kernel/kernel.py
@@ -99,11 +99,10 @@ class Kernel():
         self.K += self.K_coag
         self.K += self.K_frag
 
-        # self.K /= (self.R_coag + self.R_frag)
-        # self.K /= R_coll
         # a = self.R_coag + self.R_frag
         # b = R_coll
         # assert abs(a/b - 1).all() < 1e-14
+        # self.K /= R_coll
 
     def _K_coag(
         self, 
