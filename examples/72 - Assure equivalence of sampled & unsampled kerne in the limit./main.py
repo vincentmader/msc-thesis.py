f8adca8e64563d2ea225a5336ae078006b5144a3
Vincent C. Mader <vincent.mader@icloud.com>
2023-10-07 17:47:52 +0200

    DOC Add comments

--- examples/72 - Assure equivalence of sampled & unsampled kerne in the limit./main.py
+++ examples/72 - Assure equivalence of sampled & unsampled kerne in the limit./main.py
@@ -9,6 +9,7 @@ try:
 except ModuleNotFoundError as e:
     raise e
 
+# Define configuration.
 N_m = 50
 cfg = Config(
     mass_resolution=N_m,
@@ -16,16 +17,19 @@ cfg = Config(
 )
 mg = DiscreteMassAxis(cfg)
 
+# Define number of particles per bin. (-> Here: 1)
 N = np.ones(shape=(N_m))
 
+# Define kernels, both with & without sampling.
 kernel_1 = Kernel(cfg)
-kernel_2 = SampledKernel(cfg, N)
-
 K = kernel_1.K
+kernel_2 = SampledKernel(cfg, N)
 S = kernel_2.K
 
+# Assert equality.
 assert (K == S).all()
 
+# Plot the kernels, & their behavior re: mass error.
 plot_kernel_gain_loss(cfg, mg, kernel_1, z_limits=(1e-20, 1), scale="log")
 plot_kernel_gain_loss(cfg, mg, kernel_2, z_limits=(1e-20, 1), scale="log")
 plot_kernel_error(cfg, mg, kernel_1, scale="log", z_limits=(1e-20, 1))
