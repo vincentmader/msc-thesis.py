76db410b3a8c3baca62fdffb5cf2add23292b35c
Vincent C. Mader <vincent.mader@icloud.com>
2023-11-13 00:49:48 +0100

    MINOR Add/rewrite assertions

--- src/kernel/mass_conservation.py
+++ src/kernel/mass_conservation.py
@@ -5,14 +5,19 @@ from utils.functions import is_cubic
 
 def test_mass_conservation(mg, K, R_coll):  # TODO Turn this function into `Kernel` class method.
 
+    # Assert correct shape of kernel matrix.
     assert is_cubic(K)
     N_m = K.shape[0]
     assert N_m == mg.N
 
+    # Assert correct spacing on discretized mass axis.
     mc, dm = mg.bin_centers, mg.bin_widths
     if mg.scale == "lin":
         msg = f"dm_k != 1 on linear grid, is this on purpose?"
         assert np.abs(dm - 1).all() == 0, msg
+
+    # Assert non-negative collision rates.
+    assert R_coll.all() >= 0
 
     err_matrix = np.zeros(shape=[N_m] * 2)
     for i in range(N_m):
