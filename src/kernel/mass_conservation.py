9d221916c4645608ff7cf2b3f386b40187e4a32a
Vincent C. Mader <vincent.mader@icloud.com>
2023-11-13 00:54:10 +0100

    IMPR Rename variables

--- src/kernel/mass_conservation.py
+++ src/kernel/mass_conservation.py
@@ -3,7 +3,8 @@ import numpy as np
 from utils.functions import is_cubic
 
 
-def test_mass_conservation(mg, K, R_coll):  # TODO Turn this function into `Kernel` class method.
+def test_mass_conservation(mg, K, R_coll):  
+    # TODO Turn this function into `Kernel` class method.
 
     # Assert correct shape of kernel matrix.
     assert is_cubic(K)
@@ -19,17 +20,18 @@ def test_mass_conservation(mg, K, R_coll):  # TODO Turn this function into `Kern
     # Assert non-negative collision rates.
     assert R_coll.all() >= 0
 
-    err_matrix = np.zeros(shape=[N_m] * 2)
+    # Initialize 2d matrix for error in collision `(i, j)`.
+    E = np.zeros(shape=[N_m] * 2)
+    # Calculate error for all collisions `(i, j)`.
     for i in range(N_m):
-        for j in range(N_m):  # TODO Use `i+1` as max.?
+        for j in range(N_m):  # <- TODO Use `i+1` as max.?
             m_tot = mc[i] + mc[j]
 
-            err_ij = 0
+            E_ij = 0
             for k in range(N_m):
-                err_ij += mc[k] * K[k, i, j]
-            err_matrix[i, j] = np.abs(err_ij / m_tot) / R_coll[i, j]
-            #                TODO ^ Remove `abs()` here, plot +/- side by side.
+                E_ij += mc[k] * K[k, i, j]
+            E[i, j] = np.abs(E_ij / m_tot) / R_coll[i, j]
+            #  TODO ^ Remove `abs()` here, plot +/- side by side.
 
-    err_total = np.sum(err_matrix**2)**.5
-    # err_total = np.sum(err_matrix)
-    return err_matrix, err_total
+    E_tot = np.sum(E**2)**.5
+    return E, E_tot
