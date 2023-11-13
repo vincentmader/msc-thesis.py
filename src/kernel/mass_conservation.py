FIX wrong units/definition in title of kernel mass error plot

src/kernel/mass_conservation.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+from axis import KernelErrorVariant
 from utils.functions import is_cubic
 
 
-def test_mass_conservation(mg, K, R_coll):  
+def test_mass_conservation(mg, K, R_coll, kernel_error_variant):  
     # TODO Turn this function into `Kernel` class method.
 
     # PERFORM ASSERTIONS.
@@ -37,8 +38,12 @@ def test_mass_conservation(mg, K, R_coll):
             E_ij = 0
             for k in range(N_m):
                 E_ij += mc[k] * K[k, i, j]
-            E[i, j] = np.abs(E_ij / m_tot) / R_coll[i, j]
+            E[i, j] = np.abs(E_ij)
             #  TODO ^ Remove `abs()` here, plot +/- side by side.
+            if kernel_error_variant == KernelErrorVariant.KgPerCollision:
+                E[i, j] /= R_coll[i, j]
+            if kernel_error_variant == KernelErrorVariant.PercentPerCollision:
+                E[i, j] /= R_coll[i, j] * m_tot * 100
 
     # Calculate total error.
     E_tot = np.sum(E**2)**.5

src/visualization/kernel/kernel_mass_conservation_subplot.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 import numpy as np
 
-from axis import DiscreteMassAxis, AxisLabelVariant
+from axis import DiscreteMassAxis, AxisLabelVariant, KernelErrorVariant
 from config import Config
 from dust import particle_mass_from_radius
 from kernel.mass_conservation import test_mass_conservation
@@ -19,16 +19,32 @@ class KernelMassConservationSubplot(KernelSubplot):
         K: np.ndarray,
         R_coll: np.ndarray,
         axis_label_variant: Optional[AxisLabelVariant] = AxisLabelVariant.Radius,
+        kernel_error_variant: Optional[KernelErrorVariant] = KernelErrorVariant.PercentPerCollision,
         *args, **kwargs
     ):
-        err_matrix, err_total = test_mass_conservation(mg, K, R_coll)
+        err_matrix, err_total = test_mass_conservation(mg, K, R_coll, kernel_error_variant)
 
         if "title" not in kwargs.keys():
-            kwargs["title"]  = r"error $\Delta K_{ij}=\sum_k \frac{m_k}{m_i+m_j}\cdot\frac{K_{kij}}{R_{ij}}$, "
-            kwargs["title"] += r"$\Delta K=\sqrt{\sum_{ij}\Delta K_{ij}^2}$ = " 
-            kwargs["title"] += f"{err_total:.2e}" + " m$^3$s$^{-1}$"
+            kwargs["title"] = self.format_title(err_total, kernel_error_variant)
 
         super().__init__(cfg, mg, err_matrix, axis_label_variant=axis_label_variant, *args, **kwargs)
+
+    def format_title(self, err_total, kernel_error_variant):
+        if kernel_error_variant is KernelErrorVariant.KgPerSecond:
+            dK_ij = r"$\sum_k m_k\cdot K_{kij}$"
+            unit = "m$^3$ kg s$^{-1}$"
+        elif kernel_error_variant is KernelErrorVariant.KgPerCollision:
+            dK_ij = r"$\sum_k m_k\cdot\frac{K_{kij}}{R_{ij}}$"
+            unit = "m$^3$ kg"
+        elif kernel_error_variant is KernelErrorVariant.PercentPerCollision:
+            dK_ij = r"$\sum_k \frac{m_k}{m_i+m_j}\cdot\frac{K_{kij}}{R_{ij}}$"
+            unit = "m$^3$"
+        else:
+            raise Exception("NOTE: This can never happen.")
+
+        dK = r"$\Delta K=\sqrt{\sum_{ij}\Delta K_{ij}^2}$ = " 
+        title = r"$\Delta K_{ij}$ = " + f"{dK_ij}" + ", " + f"{dK} = {err_total:.2e} {unit}"
+        return title
 
     def format_coord(self, x, y):
         try: