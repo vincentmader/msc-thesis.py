7b0d07457b8a4916955b78bc08bbc44b800b8cac
Vincent C. Mader <vincent.mader@icloud.com>
2023-11-23 15:53:24 +0100

    MINOR Clean up

---
+++ testing/2023-11-13_02_discovery_of_kernel_asymmetry/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append("../../src")
+import numpy as np
+from axis import DiscreteMassAxis
+from config import Config
+from kernel import Kernel
+
+# ─────────────────────────────────────────────────────────────────────────────
+
+cfg = Config(
+    enable_coagulation=True,
+    enable_fragmentation=True,
+)
+mg = DiscreteMassAxis(cfg)
+
+# ─────────────────────────────────────────────────────────────────────────────
+
+N_included = 20
+ijs = [(i, j) for i in range(mg.N) for j in range(20)]
+# NOTE: I am deliberately NOT including all `(j,i)` for each `(i,j)`
+#    -> Set `N_included = mg.N`, and the error disappears.
+# NOTE: If this doesn't work: Asymmetry is handled in the kernel definition.
+
+# ─────────────────────────────────────────────────────────────────────────────
+
+R = np.ones(shape=(mg.N, mg.N))
+kernel = Kernel(
+    cfg, 
+    ijs=ijs, 
+    # R_coag=R, R_frag=R,
+)
+R = kernel.R_coag + kernel.R_frag
+
+# ─────────────────────────────────────────────────────────────────────────────
+
+scale = "log"
+z_limits = (1e-20, 1)
+
+from visualization.preset.p1 import plot_kernel_gain_loss
+plot_kernel_gain_loss(cfg, mg, kernel, scale, z_limits)
+
+from visualization.preset.p1 import plot_kernel_error
+plot_kernel_error(cfg, mg, kernel, R, scale, z_limits, symmetrized=False)
