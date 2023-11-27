05ff9021d59cc515eb8d8c25740b25abeba83b24
Vincent C. Mader <vincent.mader@icloud.com>
2023-11-27 22:40:29 +0100

    CHORE Move helper functions to `functions/solver`

--- src/models/solver/solver.py
+++ src/models/solver/solver.py
@@ -11,6 +11,7 @@ from functions.plotting.preset.p2 import plot_kernel_mass_error_vs_time
 from functions.plotting.preset.p2 import plot_kernel_sampled_vs_unsampled
 from functions.plotting.preset.p2 import plot_sampling_probability_vs_time
 from functions.plotting.preset.p2 import plot_sampling_count_vs_time
+from functions.solver import fix_negative_densities
 from models.axis import DiscreteTimeAxis, DiscreteMassAxis
 from models.kernel import Kernel, SampledKernel
 
@@ -147,28 +148,3 @@ class Solver:
                 plot_kernel_mass_error_vs_time(self.cfg, mg, Ks, R_coll)
 
         return N_dust_store, f, m2f, dm2fdt
-
-def fix_negative_densities(
-    mc:     np.ndarray,
-    N_dust: np.ndarray,
-):
-    idx_i = np.where(N_dust < 0)
-    idx_k = np.where(N_dust >= 0)
-
-    M_i = np.sum(N_dust[idx_i] * mc[idx_i])
-    M_k = np.sum(N_dust[idx_k] * mc[idx_k])
-
-    fac = np.abs(M_i) / np.abs(M_k)
-
-    N_dust[idx_k] *= 1 - fac
-    N_dust[idx_i] = 0
-
-    return N_dust
-
-def deriv(
-    y: np.ndarray,
-    x: np.ndarray,
-):
-    dx = x[1:] - x[:-1]
-    dy = y[1:] - y[:-1]
-    return dy / dx
