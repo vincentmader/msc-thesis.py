27acecca538ee4ae3787ac74b02e5753e60daddd
Vincent C. Mader <vincent.mader@icloud.com>
2023-11-27 19:38:21 +0100

    MINOR Sort

--- src/models/axis/__init__.py
+++ src/models/axis/__init__.py
@@ -1,6 +1,6 @@
+from .axis_label_variant import AxisLabelVariant
+from .axis_label_variant import KernelErrorVariant  # TODO Move
 from .discrete_axis import DiscreteAxis
 from .discrete_mass_axis import DiscreteMassAxis
 from .discrete_radial_axis import DiscreteRadialAxis
 from .discrete_time_axis import DiscreteTimeAxis
-from .axis_label_variant import AxisLabelVariant
-from .axis_label_variant import KernelErrorVariant  # TODO Move
